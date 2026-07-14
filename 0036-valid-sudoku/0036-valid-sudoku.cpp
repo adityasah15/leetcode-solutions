780c7edffd7143b2703ff6494269ecdf266373e7
adityasah15 <163900214+adityasah15@users.noreply.github.com>
2026-07-14 19:53:48 +0530

    Time: 12 ms (9.79%), Space: 25.6 MB (20.93%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.cpp
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -3,20 +3,20 @@ public:
     bool isValidSudoku(vector<vector<char>>& board) {
         unordered_set<int> rows[9];
         unordered_set<int> cols[9];
-        map<pair<int, int>, unordered_set<int>> squares;
+        unordered_set<int> squares[9];
         for (int r = 0; r < 9; r++) {
             for (int c = 0; c < 9; c++) {
-                int curr = board[r][c];
-                if (curr == '.')
+                int el = board[r][c];
+                if (el == '.')
                     continue;
-                pair<int, int> superkey = {r / 3, c / 3};
-                if (rows[r].count(curr) || cols[c].count(curr) ||
-                    squares[superkey].count(curr)) {
+                int box = (r / 3) * 3 + (c / 3);
+                if (rows[r].count(el) || cols[c].count(el) ||
+                    squares[box].count(el)) {
                     return false;
                 }
-                rows[r].insert(curr);
-                cols[c].insert(curr);
-                squares[superkey].insert(curr);
+                rows[r].insert(el);
+                cols[c].insert(el);
+                squares[box].insert(el);
             }
         }
         return true;
