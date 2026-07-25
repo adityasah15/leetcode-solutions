4e0a77ad8778833442eaa0f0bcaa29959b9a4b6c
adityasah15 <163900214+adityasah15@users.noreply.github.com>
2026-07-25 23:05:46 +0530

    Time: 0 ms (100%), Space: 10 MB (44.9%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -1,27 +1,33 @@
 class Solution {
 public:
     bool checkInclusion(string s1, string s2) {
-        int winSize = s1.size();
-        int i = 0;
-        int j = s1.size() - 1;
-        unordered_map<char, int> freq;
+        if (s1.size() > s2.size())
+            return false;
+        int freq1[26] = {0};
+        int freq2[26] = {0};
+        int winsize = s1.size();
+
+        for (int i = 0; i < winsize; i++) {
+            freq1[s1[i] - 'a']++;
+            freq2[s2[i] - 'a']++;
+        }
+        int i = 0, j = winsize - 1;
         while (j < s2.size()) {
-            for (int k = i; k <= j; k++) {
-                freq[s2[k]]++;
-                freq[s1[k - i]]--;
-            }
-            bool loopcompleted = true;
-            for (auto [el, count] : freq) {
-                if (count != 0) {
-                    i++;
-                    j++;
-                    freq.clear();
-                    loopcompleted = false;
+            bool same = true;
+            for (int k = 0; k < 26; k++) {
+                if (freq1[k] != freq2[k]) {
+                    same = false;
                     break;
                 }
             }
-            if (loopcompleted)
+            if (same) {
                 return true;
+            }
+            freq2[s2[i] - 'a']--;
+            i++;
+            j++;
+            if (j < s2.size())
+                freq2[s2[j] - 'a']++;
         }
         return false;
     }
