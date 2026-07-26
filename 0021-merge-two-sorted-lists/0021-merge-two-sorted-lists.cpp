66d307daec55aeeecd782285d70c3a5a86a06a50
adityasah15 <163900214+adityasah15@users.noreply.github.com>
2026-07-26 15:21:03 +0530

    Time: 0 ms (100%), Space: 19.6 MB (9.12%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -11,25 +11,26 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        if(list1 == nullptr) return list2;
-        if(list2 == nullptr) return list1;
-        ListNode* head = (list1->val <= list2->val)? list1 : list2;
-        ListNode* tail = head;
-        ListNode* t1 = (list1 == head)? list1->next: list1;
-        ListNode* t2 = (list2 == head)? list2->next: list2;
-        
-        while (t1 && t2){
-            if(t1->val <= t2->val){
-                tail->next = t1;  
+        if (list1 == nullptr)
+            return list2;
+        if (list2 == nullptr)
+            return list1;
+        ListNode dummy(-1);
+        ListNode* tail = &dummy;
+        ListNode* t1 = list1;
+        ListNode* t2 = list2;
+
+        while (t1 && t2) {
+            if (t1->val <= t2->val) {
+                tail->next = t1;
                 t1 = t1->next;
-            } else{
+            } else {
                 tail->next = t2;
                 t2 = t2->next;
             }
             tail = tail->next;
         }
-        if(t1 == nullptr) tail->next = t2;
-        if(t2 == nullptr) tail->next = t1;
-        return head;
+        tail->next = t1 ? t1 : t2;
+        return dummy.next;
     }
 };
