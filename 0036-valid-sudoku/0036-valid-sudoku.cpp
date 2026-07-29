217982c801f5c535286123548cb88a727af82a32
adityasah15 <163900214+adityasah15@users.noreply.github.com>
2026-07-29 21:02:42 +0530

    Time: 2 ms (58.6%), Space: 25.9 MB (9.78%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.cpp
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -6,17 +6,18 @@ public:
         unordered_set<int> squares[9];
         for (int r = 0; r < 9; r++) {
             for (int c = 0; c < 9; c++) {
+                int box = (r / 3) * 3 + (c / 3);
                 int el = board[r][c];
                 if (el == '.')
                     continue;
-                int box = (r / 3) * 3 + (c / 3);
                 if (rows[r].count(el) || cols[c].count(el) ||
                     squares[box].count(el)) {
                     return false;
+                } else {
+                    rows[r].insert(el);
+                    cols[c].insert(el);
+                    squares[box].insert(el);
                 }
-                rows[r].insert(el);
-                cols[c].insert(el);
-                squares[box].insert(el);
             }
         }
         return true;
