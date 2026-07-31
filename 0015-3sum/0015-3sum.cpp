e81b3d9de91bbf98c9d3b30b27fcdd5fb457b03b
adityasah15 <163900214+adityasah15@users.noreply.github.com>
2026-07-31 20:54:42 +0530

    Time: 52 ms (34.72%), Space: 29.2 MB (47.45%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -3,27 +3,32 @@ public:
     vector<vector<int>> threeSum(vector<int>& nums) {
         vector<vector<int>> ans;
         sort(nums.begin(), nums.end());
-        for (int i = 0; i < nums.size() - 2; i++) {
-            if (nums[i] > 0)
-                break;
+        for (int i = 0; i < nums.size(); i++) {
             if (i > 0 && nums[i] == nums[i - 1])
                 continue;
-            int l = i + 1;
-            int h = nums.size() - 1;
-            while (l < h) {
-                if (nums[i] + nums[l] + nums[h] == 0) {
-                    ans.push_back({nums[i], nums[l], nums[h]});
-                    l++;
-                    h--;
-                    while (l < h && nums[l] == nums[l - 1])
-                        l++;
-                    while (l < h && nums[h] == nums[h + 1])
-                        h--;
-
-                } else if (nums[i] + nums[l] + nums[h] < 0) {
-                    l++;
+            int low = i + 1, high = nums.size() - 1;
+            while (low < high) {
+                int sum = nums[i] + nums[low] + nums[high];
+                if (sum < 0) {
+                    low++;
+                    while (low < high && nums[low] == nums[low - 1]) {
+                        low++;
+                    }
+                } else if (sum > 0) {
+                    high--;
+                    while (low < high && nums[high] == nums[high + 1]) {
+                        high--;
+                    }
                 } else {
-                    h--;
+                    ans.push_back({nums[i], nums[low], nums[high]});
+                    low++;
+                    high--;
+                    while (low < high && nums[low] == nums[low - 1]) {
+                        low++;
+                    }
+                    while (low < high && nums[high] == nums[high + 1]) {
+                        high--;
+                    }
                 }
             }
         }
