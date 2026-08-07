5858b65c48c11fa2e2969763d01fc0aae04d279f
adityasah15 <163900214+adityasah15@users.noreply.github.com>
2026-08-07 20:21:43 +0530

    Time: 0 ms (100%), Space: 15.8 MB (49.52%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -2,21 +2,24 @@ class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
         int toswap = -1;
-        for (int i = nums.size() - 1; i > 0; i--) {
-            if (nums[i - 1] < nums[i]) {
-                toswap = i - 1;
+        int n = nums.size();
+        for(int i = n-1; i >= 1; i--){
+            if(nums[i-1] >= nums[i]){
+                continue;
+            }
+            toswap = i - 1;
+            break;
+        }
+        if(toswap == -1){
+           return reverse (nums.begin(), nums.end());
+        }
+        for (int i = n-1; i > toswap; i--){
+            if(nums[i] > nums[toswap]){
+                swap(nums[i], nums[toswap]);
+                reverse (nums.begin() + toswap + 1, nums.end());
                 break;
             }
         }
-        if (toswap == -1) {
-            return reverse(nums.begin(), nums.end());
-        }
-        for (int i = nums.size() - 1; i > toswap; i--) {
-            if (nums[i] > nums[toswap]) {
-                swap(nums[toswap], nums[i]);
-                break;
-            }
-        }
-        reverse(nums.begin() + toswap + 1, nums.end());
+        
     }
 };
