6132af8a111c5656e9d6cd022f10c1cb30f81e98
adityasah15 <163900214+adityasah15@users.noreply.github.com>
2026-08-08 20:14:27 +0530

    Time: 51 ms (42.73%), Space: 28.9 MB (99.38%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -3,32 +3,30 @@ public:
     vector<vector<int>> threeSum(vector<int>& nums) {
         vector<vector<int>> ans;
         sort(nums.begin(), nums.end());
-        for (int i = 0; i < nums.size(); i++) {
+        for (int i = 0; i < nums.size() - 2; i++) {
+            if (nums[i] > 0)
+                break;
             if (i > 0 && nums[i] == nums[i - 1])
                 continue;
             int low = i + 1, high = nums.size() - 1;
             while (low < high) {
                 int sum = nums[i] + nums[low] + nums[high];
-                if (sum < 0) {
-                    low++;
-                    while (low < high && nums[low] == nums[low - 1]) {
-                        low++;
-                    }
-                } else if (sum > 0) {
-                    high--;
-                    while (low < high && nums[high] == nums[high + 1]) {
-                        high--;
-                    }
-                } else {
+                if (sum == 0) {
                     ans.push_back({nums[i], nums[low], nums[high]});
                     low++;
                     high--;
-                    while (low < high && nums[low] == nums[low - 1]) {
+                    while (low < high && nums[low] == nums[low - 1])
                         low++;
-                    }
-                    while (low < high && nums[high] == nums[high + 1]) {
+                    while (low < high && nums[high] == nums[high + 1])
                         high--;
-                    }
+                } else if (sum < 0) {
+                    low++;
+                    while (low < high && nums[low] == nums[low - 1])
+                        low++;
+                } else {
+                    high--;
+                    while (low < high && nums[high] == nums[high + 1])
+                        high--;
                 }
             }
         }
