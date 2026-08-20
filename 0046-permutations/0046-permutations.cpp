bd6f0544ecec7910df66f12eb0d5558e0ca8ab0d
adityasah15 <163900214+adityasah15@users.noreply.github.com>
2026-08-20 21:12:26 +0530

    Time: 0 ms (100%), Space: 10.5 MB (89.07%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -2,27 +2,18 @@ class Solution {
 public:
     vector<vector<int>> permute(vector<int>& nums) {
         vector<vector<int>> ans;
-        vector<int> ds;
-        unordered_set<int> st;
-        int n = nums.size();
-        solve(ds, nums, st, ans, n);
+        solve(0, nums, ans);
         return ans;
     }
-
-    void solve(vector<int>& ds, vector<int>& nums, unordered_set<int>& st,
-               vector<vector<int>>& ans, int n) {
-        if (ds.size() == n) {
-            ans.push_back(ds);
+    void solve(int idx, vector<int>& nums, vector<vector<int>>& ans) {
+        if (idx == nums.size()) {
+            ans.push_back(nums);
             return;
         }
-        for (int i = 0; i < n; i++) {
-            if (!st.count(nums[i])) {
-                ds.push_back(nums[i]);
-                st.insert(nums[i]);
-                solve(ds, nums, st, ans, n);
-                ds.pop_back();
-                st.erase(nums[i]);
-            }
+        for (int i = idx; i < nums.size(); i++) {
+            swap(nums[i], nums[idx]);
+            solve(idx + 1, nums, ans);
+            swap(nums[i], nums[idx]);
         }
     }
 };
