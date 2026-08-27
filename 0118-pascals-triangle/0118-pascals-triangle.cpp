374c38b5cf9decf4f78f17713a89b3b799b11495
adityasah15 <163900214+adityasah15@users.noreply.github.com>
2026-08-27 21:19:57 +0530

    Time: 0 ms (100%), Space: 9.8 MB (9.42%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.cpp
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -4,8 +4,8 @@ public:
         vector<vector<int>> ans(numRows);
         for (int i = 0; i < numRows; i++) {
             ans[i].resize(i + 1);
-            for (int j = 0; j < ans[i].size(); j++) {
-                if (j == 0 || j == ans[i].size() - 1) {
+            for (int j = 0; j < i + 1; j++) {
+                if (j == 0 || j == i) {
                     ans[i][j] = 1;
                 } else {
                     ans[i][j] = ans[i - 1][j] + ans[i - 1][j - 1];
