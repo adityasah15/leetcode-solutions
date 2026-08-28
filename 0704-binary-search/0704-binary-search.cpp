7b8b12a59cc5bd987e2d42d198f90148475442d5
adityasah15 <163900214+adityasah15@users.noreply.github.com>
2026-08-28 23:32:49 +0530

    Fix formatting of high variable assignment in binary search

--- 0704-binary-search/0704-binary-search.cpp
+++ 0704-binary-search/0704-binary-search.cpp
@@ -10,7 +10,7 @@ public:
             if (nums[mid] < target) {
                 low = mid + 1;
             } else {
-                high = mid - 1;
+                high = mid-1;
             }
         }
         return -1;
